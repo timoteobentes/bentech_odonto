c2c4010269c3b3a551f4561c626bbce202ac10ed
Timóteo Freires de Souza Bentes <bentestimoteo@gmail.com>
2022-08-30 16:09:50 -0300

    loading

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// $(".Main").hide();
+
+// setTimeout(function() {
+//     $(".start").hide();
+//     $(".Main").show();
+// }, 3000);
+
+$(".start").hide();
+
+$('.nav a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+
+    $('html, body').animate({
+        scrollTop: targetOffset - 100
+    }, 500);
+});
